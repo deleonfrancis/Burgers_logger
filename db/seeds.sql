3c847c5e3c721dd612da757e5dded2d6ead90eae
Deleon Francis <dxfrancis.coding@gmail.com>
2021-02-01 16:29:59 -0500

    refactored controller and models folders

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Molly');
-INSERT INTO cats (name) VALUES ('Charlie');
-INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
-INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
-INSERT INTO cats (name, sleepy) VALUES ('Smudge', true);
-INSERT INTO cats (name) VALUES ('Daisy');
+INSERT INTO burger (name) VALUES ('In-N-Out Burger');
+INSERT INTO burger (name) VALUES ('Santa Fe Bite Burger');
+INSERT INTO burger (name, eaten) VALUES ('Craigie on Main Burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Husk Burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Salt & Time Burger', true);
+INSERT INTO burger (name) VALUES ('Daisy Burger');
